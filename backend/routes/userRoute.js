a73ecdb2ed96d3698a8533e8bebccfafc8438c6d
Deepaks1827 <deepak81unnao@gmail.com>
2024-03-23 15:40:46 +0530

    addimg features of like dislike

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Login, Logout, Register, bookmark, getMyProfile } from "../controllers/userController.js";
+import { Login, Logout, Register, bookmark, follow, getMyProfile, getOtherUser, unFollow } from "../controllers/userController.js";
 import isAuthenticated from "../config/auth.js";
 
 
@@ -9,6 +9,9 @@ router.route("/login").post(Login);
 router.route("/logout").get(Logout);
 router.route("/bookmark/:id").put(isAuthenticated,bookmark);
 router.route("/profile/:id").get(isAuthenticated,getMyProfile);
+router.route("/otheruser/:id").get(isAuthenticated,getOtherUser);
+router.route("/follow/:id").post(isAuthenticated,follow);
+router.route("/unfollow/:id").post(isAuthenticated,unFollow);
 
 
 export default router;
