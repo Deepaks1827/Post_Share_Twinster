5aa66545c21b99e4debdd782f7755786b97705a1
Deepaks1827 <deepak81unnao@gmail.com>
2024-03-24 03:08:19 +0530

    adding all the api's

--- backend/routes/tweetRoute.js
+++ backend/routes/tweetRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { createTweet, deleteTweet, likeOrDislike } from "../controllers/tweetController.js";
+import { createTweet, deleteTweet, getAllTweet, getFollowingTweets, likeOrDislike } from "../controllers/tweetController.js";
 import isAuthenticated from "../config/auth.js";
 
 
@@ -9,5 +9,7 @@ const router = express.Router();
 router.route("/create").post( isAuthenticated,createTweet);
 router.route("/delete/:id").delete(deleteTweet);
 router.route("/like/:id").put(isAuthenticated,likeOrDislike);
+router.route("/alltweets/:id").get(isAuthenticated,getAllTweet);
+router.route("/followingtweets/:id").get(isAuthenticated,getFollowingTweets);
 
 export default router;
